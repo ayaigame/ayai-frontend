e99b636397c89615d471a72530628dc055854801
oruu <orunin@gmail.com>
2013-11-01 13:43:27 -0400

    some changes

--- js/game/GameState/GameStateInterface.js
+++ js/game/GameState/GameStateInterface.js
@@ -1,27 +1,26 @@
 function GameStateInterface() {
 
-	
+    function receiveEntitiesInRoom() {
+    
+    }
+
+    function sendInputToGameState(inputEvent) {
+
+    }
 
 
+    function sendRoomId(roomId) {
+    	
+    }
 
-
-	function getEntitiesInRoom(roomId) {
-
-	}
-
-	function sendInputToGameState(inputEvent) {
-
-	}
-
-
-	function update() {
-		requestAnimFrame(animate);
+    function update() {
+        requestAnimFrame(animate);
         renderer.render(stage);
         kd.tick();
-	}
+    }
 
-	    // test movement
-    function registerKeyEvents() {
+   // test movement
+   function registerKeyEvents() {
         kd.W.down(function(e) { player.position.y -= 2; });
         kd.A.down(function(e) { player.position.x -= 2; });
         kd.S.down(function(e) { player.position.y += 2; });

8737af248cd79bd76b7e79691789fe5dd7b878a8
Ryan Lerch <lerchr@gmail.com>
2013-11-01 08:58:49 -0400

    whoopsie daisey

--- js/game/GameState/GameStateInterface.js
+++ js/game/GameState/GameStateInterface.js
@@ -1,12 +1,32 @@
 function GameStateInterface() {
+
 	
+
+
+
+
 	function getEntitiesInRoom(roomId) {
 
 	}
 
 	function sendInputToGameState(inputEvent) {
-		
+
 	}
+
+
+	function update() {
+		requestAnimFrame(animate);
+        renderer.render(stage);
+        kd.tick();
+	}
+
+	    // test movement
+    function registerKeyEvents() {
+        kd.W.down(function(e) { player.position.y -= 2; });
+        kd.A.down(function(e) { player.position.x -= 2; });
+        kd.S.down(function(e) { player.position.y += 2; });
+        kd.D.down(function(e) { player.position.x += 2; });
+    }
 
 
 }

ec6b57edc3734376953c6318bb952504ca7effa1
Timothy Hahn <timyhahn@gmail.com>
2014-03-18 17:21:06 -0400

    Implemented create ai message

--- debug.js
+++ debug.js
@@ -27,6 +27,6 @@ $(document).ready(function() {
 
 		else
 			ayai.verboseLogger = false;
-	})
+  });
 
 });
--- js/ayai/Ayai.js
+++ js/ayai/Ayai.js
@@ -1,6 +1,6 @@
 
-define("Ayai", ["phaser", "InputHandler", "Connection", "GameStateInterface", "Inventory", "Chat", "QuestLog", "AcceptQuest"], 
-    function(Phaser, InputHandler, Connection, GameStateInterface, Inventory, Chat, QuestLog, AcceptQuest) {
+define("Ayai", ["phaser", "InputHandler", "Connection", "GameStateInterface", "Inventory", "Chat", "QuestLog", "AcceptQuest", "CreateAIMessage"], 
+    function(Phaser, InputHandler, Connection, GameStateInterface, Inventory, Chat, QuestLog, AcceptQuest, CreateAIMessage) {
     // constructor
 
     ayai = Ayai.prototype;
@@ -28,6 +28,12 @@ define("Ayai", ["phaser", "InputHandler", "Connection", "GameStateInterface", "I
     };
 
     
+    $('button#createAI').click(function() {
+      var message = new CreateAIMessage();
+      ayai.connection.send(message);
+      console.log("Creating AI");
+    });
+
 
     ayai.renderMap = function(tileset, tilemap, options) {
 
